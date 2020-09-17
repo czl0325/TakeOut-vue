64aafab451a503ce35e297bbd6baf47f532850d6
陈昭良 <czl2106082@163.com>
2020-09-17 15:41:15 +0800

    导入轮播图

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <slot name="left"></slot>
+    <slot name="left" class="left"></slot>
     <span class="title ellipsis">{{ title }}</span>
     <slot name="right"></slot>
   </header>
@@ -23,10 +23,29 @@ header
   height 45px
   display flex
   align-items center
+  position fixed
+  top 0
+  left 0
   .title
-    flex-grow 1
-    font-size 24px
+    position absolute
+    left 50%
+    top 50%
+    z-index 100
+    transform translate(-50%, -50%)
+    font-size 18px
     font-weight bold
-    color white
     max-width 60%
+    color white
+  .left
+    position: absolute;
+    left 10px
+    top 50%
+    transform translateY(-50%)
+    color white
+  .right
+    position: absolute;
+    right 10px
+    top 50%
+    transform translateY(-50%)
+    color white
 </style>
--- src/pages/site/Site.vue
+++ src/pages/site/Site.vue
@@ -1,12 +1,71 @@
 <template>
   <section class="site">
-    <div class="site-header"></div>
+    <Header title="首页">
+      <router-link slot="left" class="left" to="/search">
+        <i class="iconfont icon-sousuo" ></i>
+      </router-link>
+      <router-link slot="right" class="right" :to="userInfo.id?'/userinfo':'/login'">
+        <span class="header_login_text" v-if="!userInfo.id">
+          登录|注册
+        </span>
+        <span class="header_login_text" v-else>
+           <i class="iconfont icon-person"></i>
+        </span>
+      </router-link>
+    </Header>
+
+    <div class="site-container">
+      <swiper class="swiper-container" :options="swiperOption" ref="mySwiper">
+        <swiper-slide class="slide">
+          <div style="background-color: red; width: 100%;height: 100%"></div>
+        </swiper-slide>
+        <swiper-slide class="slide">
+          <div style="background-color: green; width: 100%;height: 100%"></div>
+        </swiper-slide>
+        <swiper-slide class="slide">
+          <div style="background-color: blue; width: 100%;height: 100%"></div>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
   </section>
 </template>
 
 <script>
+import Header from '../../components/NavigationBar'
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.min.css'
+
 export default {
-  name: "Site"
+  name: "Site",
+  data() {
+    return {
+      userInfo: {},
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        loop: true,
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+        },
+        //滑动速度
+        speed: 1000,
+      }
+    }
+  },
+  components: {
+    Header,
+    Swiper,
+    SwiperSlide
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  }
 }
 </script>
 
@@ -20,5 +79,14 @@ export default {
     align-items center
     background-color $green
     height 45px
+  .site-container
+    position: fixed;
+    top 45px
+    bottom 0
+    width 100%
+    .swiper-container
+      width 100%
+      height 250px
+      --swiper-pagination-color: black;
 
 </style>
