22752cf0d423595e3764d9d03732481ab3f38842
陈昭良 <czl2106082@163.com>
2020-09-18 16:11:12 +0800

    增加服务端代码

--- takeout-client/src/router/index.js
+++ takeout-client/src/router/index.js
@@ -5,6 +5,7 @@ import Site from '../pages/site/Site'
 import Profile from '../pages/profile/Profile'
 import Order from '../pages/order/Order'
 import Search from '../pages/search/Search'
+import Login from '../pages/login/Login'
 
 Vue.use(Router)
 
@@ -16,19 +17,35 @@ export default new Router({
     },
     {
       path: '/site',
-      component: Site
+      component: Site,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/order',
-      component: Order
+      component: Order,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
